dffa2b28f8895a857550b6eb9666fdd1f598d248
asfahana01 <hana17asfa@gmail.com>
2026-01-17 19:30:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const calculatorDisplay = document.querySelector("h1");
-const inputBtns = document.querySelectorAll("buttons");
+const inputBtns = document.querySelectorAll("button");
 const clearBtn = document.getElementById("clear-btn");
 
 let firstValue = 0;
 let awaitingNextValue = false;
 
-function addNumberValue(number){
-    if(awaitingNextValue){
-        calculatorDisplay.textContent = number;
-        awaitingNextValue = false;
-    }
-    else{
-         const displayValue = calculatorDisplay.textContent;
-    calculatorDisplay.textContent = 
-       displayValue === "0" ? number : displayValue + number;
-    }
+function addNumberValue(number) {
+  // Replace current display value if first value is entered
+  if (awaitingNextValue) {
+    calculatorDisplay.textContent = number;
+    awaitingNextValue = false;
+  } else {
+    // If current display value is 0, replace it, if not add number to display value
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent =
+      displayValue === "0" ? number : displayValue + number;
+  }
 }
 
-inputBtns.forEach((inputBtns)=>{
-    if(inputBtns.classList.length == 0) {
-       inputBtns,addEventListener("click",()=> addNumberValue(inputBtns.value));
-    }
+// Add Event Listeners for numbers, operators, decimal
+// Add event listeners one at a time
+inputBtns.forEach((inputBtn) => {
+  if (inputBtn.classList.length === 0) {
+    inputBtn.addEventListener("click", () => addNumberValue(inputBtn.value));
+  }
 });
