8bd356ba2584b5cbdfb1cb338b2fb53d4633318c
asfahana01 <hana17asfa@gmail.com>
2026-01-20 18:41:59 +0530

    module & functions for values

---
+++ values.js
@@ -0,0 +1,28 @@
+let firstValue = 0;
+let operatorValue = "";
+let awaitingNextValue = false;
+
+export function getFirstValue() {
+    return firstValue;
+}
+
+export function setFirstValue(value) {
+    firstValue = value;
+} 
+
+export function getAwaitingNextValue() {
+    return awaitingNextValue;
+}
+
+export function setAwaitingNextValue(value) {
+    awaitingNextValue = value;
+} 
+
+
+export function getOperatorValue() {
+    return operatorValue;
+}
+
+export function setOperatorValue(value) {
+    operatorValue = value;
+} 
