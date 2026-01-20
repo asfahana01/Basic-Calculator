5c2eed656bc6c9fbba3bf5504e6c53756c817529
asfahana01 <hana17asfa@gmail.com>
2026-01-20 19:23:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,8 @@
-const calculatorDisplay = document.querySelector("h1");
-const inputBtns = document.querySelectorAll("button");
-const clearBtn = document.getElementById("clear-btn");
-
-let firstValue = 0;
-let operatorValue = "";
-let awaitingNextValue = false;
-
-function addNumberValue(number) {
-  // Replace current display value if first value is entered
-  if (awaitingNextValue) {
-    calculatorDisplay.textContent = number;
-    awaitingNextValue = false;
-  } else {
-    // If current display value is 0, replace it, if not add number to display value
-    const displayValue = calculatorDisplay.textContent;
-    calculatorDisplay.textContent =
-      displayValue === "0" ? number : displayValue + number;
-  }
-}
-
-
-
-function useOperator(operator) {
-  const currentValue = Number(calculatorDisplay.textContent);
-  // Prevent multiple operators
-  // Show console.log() that multiple operators are being added
-  // Also assumes if the user did not added another opertor and straightaway a new value was added new value will be assigned to the display assuming a NEW CALCULATION FROM BEGINNING
-  if (operatorValue && awaitingNextValue) {
-    operatorValue = operator;
-    return;
-  }
-  // Assign firstValue if no value
-  if (!firstValue) {
-    firstValue = currentValue;
-  } else {
-    const calculation = calculate[operatorValue](firstValue, currentValue);
-    calculatorDisplay.textContent = calculation;
-    firstValue = calculation;
-  }
-  // Ready for next value, store operator
-  awaitingNextValue = true;
-  operatorValue = operator;
-}
-function addDecimal() {
-  // If operator pressed, don't add decimal
-  if (awaitingNextValue) return;
-  // If no decimal, add one
-  if (!calculatorDisplay.textContent.includes(".")) {
-    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
-  }
-}
+import { inputBtns, clearBtn } from "./selectors";
+import addDecimal from "./addDecimal.js";
+import addNumberValue from "./addNumberValue";
+import useOperator from "./useOperator.js";
+import { resetAll } from "./values.js";
 
 // Add Event Listeners for numbers, operators, decimal
 // Add event listeners one at a time
@@ -67,15 +20,6 @@ inputBtns.forEach((inputBtn) => {
     inputBtn.addEventListener("click", () => addDecimal());
   }
 });
-
-
-// Reset all values, display
-function resetAll() {
-  firstValue = 0;
-  operatorValue = "";
-  awaitingNextValue = false;
-  calculatorDisplay.textContent = "0";
-}
 
 // Event Listener
 clearBtn.addEventListener("click", resetAll);
