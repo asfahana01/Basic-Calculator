35dedbc328f2051ab3eb6ddb2d6dcfdf0dd11c94
asfahana01 <hana17asfa@gmail.com>
2026-01-20 18:41:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,18 +19,7 @@ function addNumberValue(number) {
   }
 }
 
-// Calculate first and second values depending on operator
-const calculate = {
-  "/": (firstNumber, secondNumber) => firstNumber / secondNumber,
 
-  "*": (firstNumber, secondNumber) => firstNumber * secondNumber,
-
-  "+": (firstNumber, secondNumber) => firstNumber + secondNumber,
-
-  "-": (firstNumber, secondNumber) => firstNumber - secondNumber,
-
-  "=": (firstNumber, secondNumber) => secondNumber,
-};
 
 function useOperator(operator) {
   const currentValue = Number(calculatorDisplay.textContent);
