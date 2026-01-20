fc2f2ebef0da1adebd009bba768347fe02b4958f
asfahana01 <hana17asfa@gmail.com>
2026-01-20 19:10:27 +0530

    module for addDecimal

---
+++ addDecimal.js
@@ -0,0 +1,13 @@
+import { getAwaitingNextValue } from "./values";
+import { calculatorDisplay } from "./selectors";
+
+function addDecimal() {
+  // If operator pressed, don't add decimal
+  if (getAwaitingNextValue()) return;
+  // If no decimal, add one
+  if (!calculatorDisplay.textContent.includes(".")) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
+}
+
+export default addDecimal;
